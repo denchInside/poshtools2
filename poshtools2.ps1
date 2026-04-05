ac6f6595e4e84f1a4f542449d2dc06f18c593e00
dxizx <dckick39.mc@gmail.com>
2026-04-05 16:29:47 +0200

    Add shim generator and UpdateShims category

--- poshtools2.ps1
+++ poshtools2.ps1
@@ -2,12 +2,11 @@ using namespace System.IO
 
 param(
 	[Parameter(Position = 0)]
-	[ValidateSet("Help", "Update", "ChangeDirectory", "Explorer", "DeveloperMode")]
+	[ValidateSet("Help", "Update", "UpdateShims", "ChangeDirectory", "Explorer", "DeveloperMode")]
 	[String]$Category = "Help",
 	[Parameter(Position = 1)]
 	[String]$Action
 )
-
 $ErrorActionPreference = 'Stop'
 $WarningPreference = 'SilentlyContinue'
 $GitRepo = 'http://192.168.178.42/dxizx/poshtools2.git'
@@ -60,6 +59,23 @@ function Category-Update {
 	git -C $PSScriptRoot clean -fd
 
 	Register-Path -Path $PSScriptRoot -Permanent
+	Category-UpdateShims
+}
+
+function Category-UpdateShims {
+	Import-Module "$PSScriptRoot\modules\path.psm1" -Scope Local
+	Import-Module "$PSScriptRoot\modules\shim.psm1" -Scope Local
+	
+	$shimsPath = "$PSScriptRoot\.data\shims"
+	$null = [Directory]::CreateDirectory($shimsPath)
+	
+	$shims = Get-ChildItem -LiteralPath $PSScriptRoot -File -Filter "*.ps1" |
+		ForEach-Object {
+			New-PSShim -FromFile $_ -OutputDirectory $shimsPath
+		}
+	
+	Register-Path -Path $shimsPath -Permanent
+	Write-Host $shims
 }
 
 $FunctionLookup = @{
@@ -68,6 +84,7 @@ $FunctionLookup = @{
 	"ChangeDirectory" = ${Function:Category-ChangeDirectory}
 	"DeveloperMode" = ${Function:Category-DeveloperMode}
 	"Update" = ${Function:Category-Update}
+	"UpdateShims" = ${Function:Category-UpdateShims}
 }
 
 & $FunctionLookup[$Category] $Action
